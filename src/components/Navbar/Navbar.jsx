67b1cd7ec46e4301887b381196ede0618e0983a7
immobarok <immobarokali2001@gmail.com>
2025-05-04 02:23:00 +0600

    nav and hero addeddd

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+import profile_img from '../../assets/profile_img.png'
+import caret_icon from '../../assets/caret_icon.svg'
+
+const Navbar = () => {
+   return (
+      <div className='navbar'>
+         <div className='navbar-left'>
+            <p className='logo'>STREAM ON</p>
+            <ul>
+               <li>Home</li>
+               <li>TV Show</li>
+               <li>New & Popular</li>
+               <li>My List</li>
+               <li>Browse My Languages</li>
+            </ul>
+         </div>
+         <div className='navbar-right'>
+            <div className='search-bar'>
+               <form action="">
+                  <input type="text" placeholder='Search Movies Here...' />
+                  <button>
+                     <img src={search_icon} alt="" />
+                  </button>
+               </form>
+            </div> 
+            <img className='icons' src={bell_icon} alt="" />
+            <div className="navbar-profile">
+               <img className='profile' src={profile_img} alt="" />
+               <img className='' src={caret_icon} alt="" />
+               <div className='dropdown'>
+                  <p>Sign Out of STREAMON</p>
+               </div>
+            </div>
+         </div>
+      </div>
+   )
+}
+
+export default Navbar
